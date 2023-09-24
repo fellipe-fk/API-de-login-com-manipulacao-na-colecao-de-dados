9d859d9363ee388a7bdd08e97916f16b4a8798ae
Fellipe <ramosfellipe630@gmail.com>
2023-09-23 23:30:46 -0300

    falta o verbo delete e fazer melhorias na validaçao

---
+++ index.js
@@ -0,0 +1,96 @@
+import express from 'express';
+import { retornaListaDados, buscausuariosId, cadatraUsuario, atualizarUsuarioBaseDados,atualizarUmdadosBaseDados } from './logicaAplicada/funcoes.js';
+import { validarCamposNomeSenha } from './validaErro/validaErro.js';
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+// retorna um id especifico
+app.get('/login/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    
+    if(isNaN(id)){
+        res.status(404).json({erro: 'Id invalido'});
+    }
+
+    const retornId = buscausuariosId(id);
+    if(retornId){
+        res.json(retornId);
+    }else{
+        res.status(404).json({erro: 'Id nao encontrado'});
+    };
+});
+
+//cadastra novo usuario
+app.post('/login', (req, res) => {
+    const nome = req.body.nome;
+    const senha = req.body.senha;
+    const erros = validarCamposNomeSenha(nome, senha);
+
+    if (Object.keys(erros).length > 0) {
+        res.status(400).json(erros);
+      }else{
+        const usuario = cadatraUsuario(nome, senha);
+
+        res.json(usuario);
+      }
+});
+
+//atualiza todos os dados usuario
+app.put('/login/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const nome = req.body.nome;
+    const senha = req.body.senha;
+
+    const erros = validarCamposNomeSenha(nome, senha);
+    
+    if (Object.keys(erros).length > 0) {
+        res.status(400).json(erros);
+      };
+    
+    if (isNaN(id)) {
+      return res.status(404).json({ erro: 'Id inválido' });
+    };
+  
+    const atualizadoComSucesso = atualizarUsuarioBaseDados(id, nome, senha);
+  
+    if (atualizadoComSucesso) {
+      res.json(atualizadoComSucesso);
+    } else {
+      res.status(404).json({ erro: 'Usuário não encontrado' });
+    };
+});
+
+//atualiza um dado especifico do usuario
+app.patch('/login/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const {nome, senha} = req.body;
+
+    
+    if (isNaN(id)) {
+      return res.status(404).json({ erro: 'Id inválido' });
+    };
+
+    if((nome && senha )||(!nome && !senha)){
+      res.status(400).json({erro:"Escolha nome ou senha para atualizar"});
+    };
+
+    const atualizarUsuario = atualizarUmdadosBaseDados(id,nome,senha)
+
+    if (atualizarUsuario) {
+      res.json(atualizarUsuario);
+    } else {
+      res.status(404).json({ erro: 'Usuário não encontrado' });
+    };
+});
+
+
+// retorna toda a lista
+app.get('/login', (req, res) => {
+    const lista = retornaListaDados();
+    res.json(lista);
+});
+
+app.listen(port, () => {
+    console.log('API login inicializada..')
+});
