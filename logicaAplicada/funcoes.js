b85f6d25579afa7b7588ab0eb9e00fe8cbff37ed
Fellipe <ramosfellipe630@gmail.com>
2023-09-23 23:30:15 -0300

    80% pronto

---
+++ logicaAplicada/funcoes.js
@@ -0,0 +1,66 @@
+import baseDadosUsuarios from "../dados/dados.js";
+
+//retorna toda a lista de dados
+export const retornaListaDados = () => baseDadosUsuarios;
+
+
+// busca o id dos usuario
+export const buscausuariosId  = (id) => {
+    const filtraId = parseInt(id);
+    const Id = baseDadosUsuarios.find(item => item.id === filtraId);
+    return Id;
+};
+
+
+
+//cadastra novos usuarios
+export const cadatraUsuario = (nome, senha) => {
+    const novoId = baseDadosUsuarios.reduce((maxId, usuario) => {
+        return usuario.id > maxId ? usuario.id : maxId;
+    }, 0);
+
+    const novoUsuario = {
+        id: novoId + 1,
+        nome:nome,
+        senha:senha,
+    };
+
+    baseDadosUsuarios.push(novoUsuario);
+    return novoUsuario;
+};
+
+
+
+
+//atualizar todos os usuarios
+export const atualizarUsuarioBaseDados = (id, nome, senha) => {
+    const usuarioIndex = baseDadosUsuarios.findIndex(item => item.id === id);
+  
+    if (usuarioIndex !== -1) {
+      baseDadosUsuarios[usuarioIndex].nome = nome;
+      baseDadosUsuarios[usuarioIndex].senha = senha;
+      return baseDadosUsuarios[usuarioIndex];
+    } else {
+      return false;
+    };
+};
+
+
+//atualiza um dado do usuario por vez  
+export const atualizarUmdadosBaseDados = (id, nome, senha) => {
+    const usuario = baseDadosUsuarios.find(item => item.id === id);
+
+    if (!usuario) {
+        return false;
+    }
+
+    if (nome) {
+        usuario.nome = nome;
+    }
+
+    if (senha) {
+        usuario.senha = senha;
+    }
+
+    return usuario;
+};
